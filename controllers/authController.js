7a044d346ce41980ee69e3b124b7021ff2b65a8d
puja <pujabhujbal8824@gmail.com>
2025-01-17 05:32:32 +0530

    sec commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -41,8 +41,13 @@ const register = async (req,res) =>{
 
 }
 const logout = async (req,res) =>{
-    res.send('logout user')
-}
+    res.cookie('token', 'logout', {
+        httpOnly: true,
+        expires: new Date(Date.now() + 1000),
+      });
+      res.status(StatusCodes.OK).json({ msg: 'user logged out!' });
+    
+} 
 
 module.exports = {
     register,
